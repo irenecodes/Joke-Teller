2813a693047a3436201fa4b1dd2188563af0862e
Irene <irenesis123@gmail.com>
2020-12-28 04:18:03 -0500

    added joke api, event listeners

--- script.js
+++ script.js
@@ -105,10 +105,16 @@ const VoiceRSS = {
   },
 };
 
-function tellMe() {
+// Disable/Enable button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// Passing Joke to Voice RSS API
+function tellMe(joke) {
   VoiceRSS.speech({
     key: "f518a45608e14f1faa2bef2daaa50c20",
-    src: "Hi there",
+    src: joke,
     hl: "en-us",
     v: "Linda",
     r: 0,
@@ -117,4 +123,32 @@ function tellMe() {
     ssml: false,
   });
 }
-tellMe();
+
+// Get Jokes from Jokes API
+async function getJokes() {
+  let joke = "";
+  const apiUrl = `https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit`;
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      // sets up the two-liners
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+
+    // Text-to-speech
+    tellMe(joke);
+    // Disable button
+    toggleButton();
+  } catch (error) {
+    //Catch Errors Here
+    console.log("error:", error);
+  }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+
+audioElement.addEventListener("ended", toggleButton);
